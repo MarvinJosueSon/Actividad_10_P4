d4f336ad276cad4bffa959f485a5c821cf0c2754
Marvin <sonmarvint2001@gmail.com>
2025-07-25 21:12:00 -0600

    Imprimir cuenta regresiva

--- main.py
+++ main.py
@@ -4,5 +4,20 @@ def contarDigitos(numero):
     else:
         return 1+contarDigitos(numero//10)
 
-contarDigitos(154)
-print(contarDigitos(154))
+def sumarDigitos(numero):
+    if numero<=0:
+        return numero
+    else:
+        return 1+sumarDigitos(numero%10)
+        numero=numero//10
+
+def imprimirCuentaRegresiva(numero):
+    if numero<=0:
+        return numero
+    else:
+        print(numero)
+        return imprimirCuentaRegresiva(numero-1)
+imprimirCuentaRegresiva(10)
+
+
+
